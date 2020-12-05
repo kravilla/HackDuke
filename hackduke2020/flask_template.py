2f7f33580f0fce56bacb6fd430f2d7c637b5ea32
Justin Lorenz <jml166@duke.edu>
2020-12-05 16:27:23 -0600

    testing flask

--- hackduke2020/flask_template.py
+++ hackduke2020/flask_template.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template
 from database import db_session
-from models import Car
+from models import User
 import pdb
-
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -11,43 +10,46 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('flask_index.html')
 
 
-#worth noting when we're embedding python in flask we're using something called a
-#jinja template https://jinja.palletsprojects.com/en/2.10.x/templates/
+# worth noting when we're embedding python in flask we're using something called a
+# jinja template https://jinja.palletsprojects.com/en/2.10.x/templates/
 @app.route('/python')
 def show_python():
-  return render_template(
-    'python.html',
-    car_list=["Ford", "Jaguar", "Hyundai"],
-    car_dictionary={"make": "Ford", "model": "Mustang", "year": "2019", "color":"red"}
-  )
+    return render_template(
+        'python.html',
+        car_list=["Ford", "Jaguar", "Hyundai"],
+        car_dictionary={"make": "Ford", "model": "Mustang", "year": "2019", "color": "red"}
+    )
+
 
 @app.route('/database')
 def database():
-  return render_template(
-    'database.html',
-    cars = Car.query.all()
-  )
+    return render_template(
+        'database.html',
+        cars=Car.query.all()
+    )
+
 
 @app.route('/add_car')
 def add_car():
-  make = request.args.get("make")
-  model = request.args.get("model")
-  color = request.args.get("color")
-  year = request.args.get("year")
+    make = request.args.get("make")
+    model = request.args.get("model")
+    color = request.args.get("color")
+    year = request.args.get("year")
 
-  car = Car(make, model, color, year)
-  db_session.add(car)
-  db_session.commit()
+    car = Car(make, model, color, year)
+    db_session.add(car)
+    db_session.commit()
 
-  return redirect(url_for('database'))
+    return redirect(url_for('database'))
 
 
 @app.route('/style')
 def style():
     return render_template('index_with_style.html')
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
--- hackduke2020/models.py
+++ hackduke2020/models.py
@@ -1,19 +1,50 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ARRAY
 from database import Base
 
-class Car(Base):
-    __tablename__ = 'cars'
-    id = Column(Integer, primary_key=True)
-    make = Column(String(20), unique=True)
-    model = Column(String(20), unique=False)
-    color = Column(String(20), unique=False)
-    year = Column(String(4), unique=False)
 
-    def __init__(self, make=None, model=None, color=None, year=None):
-        self.make = make
-        self.model = model
-        self.color = color
-        self.year = year
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(), unique=True)
+    username = Column(String(), unique=True)
+    password = Column(String(), unique=False)
+    isMentor = Column(Boolean, unique=False)
+    name = Column(String(), unique=False)
+    preferences = Column(ARRAY(String()), unique=False)
+
+    def __init__(self, email, username, password, name, preferences, isMentor=False):
+        self.email = email
+        self.username = username
+        self.password = password
+        self.name = name
+        self.isMentor = isMentor
+        self.preferences = preferences
+
+    def changeName(self, newName):
+        self.name = newName
+
+    def changeUsername(self, newUsername):
+        self.username = newUsername
+
+    def changePassword(self, newPassword):
+        self.password = newPassword
+
+    def changeEmail(self, newEmail):
+        self.email = newEmail
+
+    def becomeMentor(self):
+        self.isMentor = True
+
+    def addPreferences(self, newPrefs):
+        self.preferences += newPrefs
+
+    def clearPrefs(self):
+        self.preferences = []
+
+    def deletePrefs(self, delPrefs):
+        for delete in delPrefs:
+            if delete in self.preferences:
+                self.preferences.remove(delete)
 
     def __repr__(self):
-        return '<Car %r>' % (self.model)
+        return ""
